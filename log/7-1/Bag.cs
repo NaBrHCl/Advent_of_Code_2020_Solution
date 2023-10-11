38923d5de9ae51da63b50687bd72f3a4237eca32
Shijian Liu <jdbkdy@gmail.com>
2023-10-11 15:22:26 -0400

    complete day 7 - part 1

---
+++ log/7-1/Bag.cs
@@ -0,0 +1,36 @@
+﻿namespace test
+{
+    internal class Bag
+    {
+        private string _color;
+        private string[] _containedColors;
+
+        public Bag(string color_, string[] containedColors_)
+        {
+            _color = color_;
+            _containedColors = containedColors_;
+        }
+
+        // Checks direct containment only
+        public bool Contains(string keyColor)
+        {
+            foreach (string color in ContainedColors)
+            {
+                if (color == keyColor)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public string Color
+        {
+            get { return _color; }
+        }
+
+        public string[] ContainedColors
+        {
+            get { return _containedColors; }
+        }
+    }
+}
